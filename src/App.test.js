4d522f6a71ac093e7854eed4ac3806be287a74a9
Jonathan Nielsen <jonathan.sparvath@gmail.com>
2022-08-23 15:15:32 +0200

    cleanup

--- src/App.test.js
+++ src/App.test.js
@@ -5,17 +5,3 @@ import App from './App';
 it('renders without crashing', () => {
   shallow(<App locale="da" />);
 });
-
-// it('renders 5 rows', () => {
-//   const wrapper = mount(<App rows={rows} locale="da" rowsPerPage={5} />);
-
-//   expect(wrapper.find('tr').length).toBe(5);
-// });
-
-// it('filters rows based on input', () => {
-//   const wrapper = mount(<App rows={rows} locale="da" rowsPerPage={5} />);
-
-//   wrapper.find('input').simulate('change', { target: { value: 'k' } });
-
-//   expect(wrapper.find('tr').length).toBe(2);
-// });
