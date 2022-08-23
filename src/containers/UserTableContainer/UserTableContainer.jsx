4fd0bb96dc9394261b290c60cf5c53b0286c07c1
Jonathan Nielsen <jonathan.sparvath@gmail.com>
2022-08-23 15:05:40 +0200

    refactor tests

--- src/containers/UserTableContainer/UserTableContainer.jsx
+++ src/containers/UserTableContainer/UserTableContainer.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { getUsers } from '../../services/userService';
-import Table from '../../components/UserTable';
+import Table from '../../components/Table';
 
 export function UserTableContainer() {
   const {
